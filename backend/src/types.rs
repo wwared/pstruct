7fb543a2ceb0c23956ddb1cb3dd693c9e1aead7a
wwared <wwared@users.noreply.github.com>
2021-03-10 12:27:48 -0300

    types: derive Debug on everything

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -1,26 +1,35 @@
-#[derive(PartialEq, Clone, Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Type<'a> {
-    U8, U16, U32, U64,
-    I8, I16, I32, I64,
-    F32, F64,
+    U8,
+    U16,
+    U32,
+    U64,
+    I8,
+    I16,
+    I32,
+    I64,
+    F32,
+    F64,
     Byte,
     String,
     CString,
     User(&'a str),
 }
 
+#[derive(Debug)]
 pub enum Array<'a> {
     Constant(usize),
     Unknown(Type<'a>),
     Variable(&'a str, Type<'a>),
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub enum Endian {
     Little,
     Big,
 }
 
+#[derive(Debug)]
 pub struct Item<'a> {
     pub name: &'a str,
     pub kind: Type<'a>,
@@ -28,11 +37,13 @@ pub struct Item<'a> {
     pub byte_order: Endian,
 }
 
+#[derive(Debug)]
 pub struct Struct<'a> {
     pub name: &'a str,
     pub items: Vec<Item<'a>>,
 }
 
+#[derive(Debug)]
 pub struct File<'a> {
     pub scope: String,
     pub structs: Vec<Struct<'a>>,
